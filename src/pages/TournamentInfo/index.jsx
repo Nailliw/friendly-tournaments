e9015ec7a1a4c6f9737db03955ebc1fbede0c8f9
Jefte Keller <kellergnr@gmail.com>
2021-01-14 02:41:46 -0300

    feat: Completed components TitleHeader and StatusSection of Tournament Info Page

--- src/pages/TournamentInfo/index.jsx
+++ src/pages/TournamentInfo/index.jsx
@@ -1,7 +1,67 @@
+import { useStyles } from "./styles";
+
+import { Box } from "@material-ui/core";
+
+import { TitleHeader } from "../../components/local/TournamentInfo/TitleHeader";
+import { StatusSection } from "../../components/local/TournamentInfo/StatusSection";
+
 export const TournamentInfo = () => {
+  const classes = useStyles();
+
+  const tournamentData = {
+    category: 10,
+    title: "The International", // .done
+    info: "The biggest esport event of the world!",
+    numberOfTeams: 30,
+    teamsSize: 5,
+    teamsData: [
+      {
+        teamName: "LA Eagles",
+        id: 2,
+      },
+      {
+        teamName: "Miami Aligators",
+        id: 3,
+      },
+    ],
+    remainingTeams: [
+      {
+        teamName: "LA Eagles",
+        id: 2,
+      },
+      {
+        teamName: "Miami Aligators",
+        id: 3,
+      },
+    ],
+    matches: [],
+    status: "Esperando Times",
+    tournamentWinner: {},
+    deadline: "2021-01-14T07:34",
+    userId: 2,
+    id: 2,
+  };
+
+  const {
+    title,
+    info,
+    numberOfTeams,
+    teamsSize,
+    teamsData,
+    status,
+    deadline,
+  } = tournamentData;
+
   return (
-    <div>
-      <h1>Tourney Info</h1>
-    </div>
+    <Box component="div" className={classes.root}>
+      <TitleHeader title={title} />
+      <StatusSection
+        numberOfTeams={numberOfTeams}
+        teamsSize={teamsSize}
+        teamsData={teamsData}
+        status={status}
+        deadline={deadline}
+      />
+    </Box>
   );
 };
---
+++ src/pages/TournamentInfo/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles(() => ({
+  root: {
+    width: "80%",
+    height: "80vh",
+    margin: "5rem auto",
+    borderRadius: "1rem",
+
+    // backgroundColor: "#15384F",
+    // backgroundColor: "#225B81",
+    // backgroundColor: "#202442",
+
+    backgroundColor: "#2A2F55",
+
+    // backgroundColor: "#7033FF",
+    // backgroundColor: "#FF7843",
+
+    // "@media (min-width: 600px)": {
+    //
+    // },
+  },
+}));
