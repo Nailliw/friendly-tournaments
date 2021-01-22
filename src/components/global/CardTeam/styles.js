f78fca6005ecd9fab483ca5b3f76285b5b252d84
Jefte Keller <kellergnr@gmail.com>
2021-01-22 09:20:45 -0300

    feat: user can enter a team, and validations to register

--- src/components/global/CardTeam/styles.js
+++ src/components/global/CardTeam/styles.js
@@ -9,23 +9,28 @@ export const useStyles = makeStyles({
       color: "#FFF",
     },
   },
+
   media: {
     height: 140,
   },
+
   contents: {
     height: "300px",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
+
   title: {
     textAlign: "center",
     marginBottom: "5%",
   },
+
   info: {
     textAlign: "center",
     marginBottom: "5%",
   },
+
   teamsPlayers: {
     width: "80%",
     display: "flex",
@@ -33,6 +38,7 @@ export const useStyles = makeStyles({
     textAlign: "center",
     marginBottom: "5%",
   },
+
   playerCard: {
     marginTop: "3%",
     marginBottom: "3%",
@@ -41,23 +47,36 @@ export const useStyles = makeStyles({
     marginTop: "10%",
     marginBottom: "1%",
   },
+
   TournamentsDisputedCard: {
     marginTop: "1%",
     marginBottom: "2%",
   },
+
   subtitle: {
     fontSize: "24px",
     marginTop: "15%",
     marginBottom: "10%",
     fontWeight: "bold",
   },
+
   slots: {
     textAlign: "center",
     marginTop: "5%",
   },
+
   seeTournament: {
     display: "flex",
     justifyContent: "center",
     alignContent: "center",
+
+    "& button": {
+      backgroundColor: "#454D60",
+      width: "80%",
+      "&:hover": {
+        backgroundColor: "#303542",
+        boxShadow: "0.1em 0.1em 0.2em black",
+      },
+    },
   },
 });
